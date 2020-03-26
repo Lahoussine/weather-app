a09196e154bce2ca3360aff6761f3202170b4e03
LDE <lahoussine.derradji@imtf.com>
2020-03-26 13:32:02 +0100

    call weather api in progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,43 @@ class Home extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {weather:[],position:[]};
+  }
+  
+  componentDidMount(){
+    console.log("Weather Data Retrieval...");
+    let lat = null;
+    let lon = null;
+    navigator.geolocation.getCurrentPosition(
+
+      position =>{console.log('position')
+       lat= position.coords.latitude;
+       lon= position.coords.longitude;
+        //meteo actuelle weather?
+        let weatherCurrent = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&units=metric&appid=526309393592f5cf6ed361609dfd8e78';
+        // forecast pour les prevision par default retourne une liste de 7
+        //let weatherForecast = 'https://api.openweathermap.org/data/2.5/forecast/daily?lat='+lat+'&lon='+lon+'&units=metric&appid=526309393592f5cf6ed361609dfd8e78';
+        //'https://api.openweathermap.org/data/2.5/forecast/daily?q=Neuchatel&units=metric&appid=526309393592f5cf6ed361609dfd8e78')
+            fetch(weatherCurrent)
+                .then(res => res.json())
+                .then((data) => {
+                  console.log(data);
+                  this.setState({ weather: data,
+                  position:position
+                  });
+                })
+                .catch(console.log('error'))
+
+      }, 
+      err => console.log(err)
+    );
+
   }
   render() {
+    console.log('render')
 
+console.log('----blabla-------');
+console.log(this.state.weather.weather);
     const styles = {
       root: {
         minWidth: 275,
@@ -29,6 +63,8 @@ class Home extends Component {
     };
 
     console.log(JSON.stringify(styles.title.fontSize));
+    
+    console.log(this.state.weather)
     return (
       <div className="content">
         <h2>Home</h2>
@@ -37,7 +73,7 @@ class Home extends Component {
         <div className="row space-between">
           <Card id="card" className="column info">
             <CardContent>
-              <Typography style={{ fontSize: JSON.stringify(styles.title.fontSize) + 'px' }} color="primary" gutterBottom>
+              <Typography component={'span'} style={{ fontSize: JSON.stringify(styles.title.fontSize) + 'px' }} color="primary" gutterBottom>
                 <div>             Information Alert      </div>
               </Typography>
               <Typography component={'span'} className={JSON.stringify(styles.pos)} gutterBottom>
@@ -45,8 +81,8 @@ class Home extends Component {
                   <div className="cardRow value ">5</div>
                   <div className="cardRow central fix debugBorderFushia">
                     <div>
-                      <div className="title">Alert</div>
-                      <div className="description" >Alert</div>
+                      <div className="title">Meteo</div>
+    <div className="description" >blabla</div>
                     </div>
                   </div>
                   <div className="cardRow icon">
