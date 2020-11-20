7945b41af92a2b3235931031f731652de426b24b
lde <Lahoussine.derradji@imtf.com>
2020-11-20 17:25:28 +0100

    add table with data

--- src/components/History.js
+++ src/components/History.js
@@ -4,6 +4,16 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import {Line} from 'react-chartjs-2';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from "@material-ui/core/styles";
+
 function formatDate(date){
   var dt = new Date(date)
   var dd = dt.getDate();
@@ -64,9 +74,37 @@ const data = {
     }
   ]
 };
+
+
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+//const classes = useStyles();
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+const styles = theme => ({
+  table: {
+    minWidth: 650,
+  },
+});
 class History extends Component {
 
   render() {
+    const { classes } = this.props;
+    {console.log(classes)}
     return (
       <div className="content">
         <h2>History</h2>
@@ -87,7 +125,35 @@ class History extends Component {
             </CardContent>
           </Card>
         </div>
-        <div className="row space-between debugBorder"></div>
+
+        <div className="row space-between debugBorder">
+        <TableContainer component={Paper}>
+      <Table className={styles.table} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+        </div>
       </div>
     );
   }
