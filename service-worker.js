d8adda4789543d22bd244943f1595ae5da6ae1c9
lde <Lahoussine.derradji@imtf.com>
2020-12-25 17:54:09 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.ea484bed1d99854e71091a5b8bde61af.js"
+  "/weather-app/precache-manifest.171fa74b5e2f72a32092c9c0e2a01c68.js"
 );
 
 self.addEventListener('message', (event) => {
