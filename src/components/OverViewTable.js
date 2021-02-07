a4dba200f8b3caab8641b5393b194174dbf3ced2
lde <Lahoussine.derradji@imtf.com>
2021-02-07 11:29:47 +0100

    table item divider

--- src/components/OverViewTable.js
+++ src/components/OverViewTable.js
@@ -2,11 +2,25 @@
 import React from 'react';
 import '.././OverViewTable.css';
 import moment from 'moment';
+
+import Divider from '@material-ui/core/Divider';
+import { makeStyles } from '@material-ui/core/styles';
+
 function OverViewTable(props) {
     const { row } = props;
 
+    const useStyles = makeStyles({
+        divider:{
+         backgroundColor:'#9A9A9A',
+         position: 'relative',       
+         width: '100px',
+         left:'135px',
+         top:'20px'
+        }
+      });
+    const classes = useStyles();
     return (
-        <React.Fragment>
+        <React.Fragment>            
             <div className="detail-row space-between">
                 <div className="detail-column">
                     <div className="detail-row2">
@@ -37,6 +51,7 @@ function OverViewTable(props) {
                     </div>
                 </div>
             </div>
+            <Divider className={classes.divider}/>
         </React.Fragment>
     );
 }
